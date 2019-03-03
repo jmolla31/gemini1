be8cfcba3e1f3d4993df4d4f8c98cba128ee2a73
Javi Mollà <jmolla31@gmail.com>
2019-03-03 19:18:26 +0100

    updated items screen

--- Hosting/public/pages/items/items.js
+++ Hosting/public/pages/items/items.js
@@ -1,1 +1,34 @@
-document.getElementById("date").value = new Date(Date.now()).toLocaleDateString();
+document.getElementById("entryDate").value = new Date(Date.now()).toLocaleDateString();
+
+document.getElementById("btnSave").addEventListener("click", x => {
+
+  console.log("duh!");
+
+  var mc = document.getElementById("mainCategory");
+  var sc = document.getElementById("secondaryCategory");
+
+  var itemObject = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value,
+    mainCategory: mc.options[mc.options.selectedIndex].value,
+    secondaryCategory: sc.options[sc.options.selectedIndex].value,
+    entryDate: document.getElementById("entryDate").value,
+    locked: document.getElementById("locked").checked
+  }
+
+  // Swal.fire({
+  //   type: 'success',
+  //   title: 'Pooh!',
+  //   text: 'Item guardat a la base de dades!',
+  // });
+
+  // Swal.fire({
+  //   type: 'error',
+  //   title: 'Ups!',
+  //   text: 'Error: {errorVariablePlaceholder}',
+  // })
+  //TODO: Post item to firebase!
+
+  console.log(itemObject);
+
+});
