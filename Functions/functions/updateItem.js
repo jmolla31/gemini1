b5d9543c06716396b41b80e944d330d4fbf7f4af
Javi Mollà <jmolla31@gmail.com>
2019-04-16 20:40:20 +0200

    Added code for updateItem.js

---
+++ Functions/functions/updateItem.js
@@ -0,0 +1,33 @@
+exports.handler = function (req, res, database) {
+
+    console.log("Function processed");
+
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Methods", "PUT");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    // Send response to OPTIONS requests and terminate the function execution
+    if (req.method == 'OPTIONS') {
+        res.status(200).send('');
+    }
+
+
+    let recievedBody = req.body;
+    console.log(recievedBody);
+
+    database.collection('items').doc(recievedBody.id).set({
+
+        name: recievedBody.name,
+        description: recievedBody.description,
+        mainCategory: recievedBody.mainCategory,
+        secondaryCategory: recievedBody.secondaryCategory,
+        entryDate: recievedBody.entryDate,
+        locked: recievedBody.locked
+    })
+        .then(function () {
+            res.status(200).send();
+        })
+        .catch(function (error) {
+            res.status(400).send("Error writing document: ", error);
+        });
+};
