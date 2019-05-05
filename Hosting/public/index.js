39a538321f463dd1638ea702b2953de25e9c48de
Javi Mollà <jmolla31@gmail.com>
2019-05-05 18:54:11 +0200

    Removed loans table from index page, truncate  new items table to 10 elements

--- Hosting/public/index.js
+++ Hosting/public/index.js
@@ -21,8 +21,7 @@ httpGetAsync(getAllItemsUrl, data => {
 
   data = JSON.parse(data);
 
-  console.log(data);
-
+  data.length = 10;
 
   var tableRef = document.getElementById('itemsTable').getElementsByTagName('tbody')[0];
 
